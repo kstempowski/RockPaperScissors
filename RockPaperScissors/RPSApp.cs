13e68020d12911894ddafbd327297c1017b3b994
Your Name <kennystemper@gmail.com>
2021-05-17 18:46:13 -0400

    seventh commit

--- RockPaperScissors/RPSApp.cs
+++ RockPaperScissors/RPSApp.cs
@@ -6,45 +6,141 @@ namespace RockPaperScissors
 {
     class RPSApp
     {
-        
+
         public static List<Player> Players()
         {
-
             List<Player> players = new List<Player> { };
+            HumanPlayer player1 = new HumanPlayer(PlayerName());
 
             bool repeat = true;
             while (repeat)
             {
+                bool playerSelect = true;
+                while (playerSelect)
+                {
                 Console.WriteLine("Who would you like to play against? Rock Player (rock), or Random Player (rand)?");
                 string opponent = Console.ReadLine().ToLower();
+                    if (opponent == "rock")
+                    {
+                        RockPlayer rocky = new RockPlayer("Rocky");
+                        Console.WriteLine("You chose to play a rock player");
+                        Console.WriteLine("Please make a selection: Rock, paper, scissors");
+                        string input = Console.ReadLine().ToLower();
 
-                if (opponent == "rock")
-                {
-                    RockPlayer rocky = new RockPlayer("Rocky");
-                    Console.WriteLine("You chose to play a rock player");
-                    repeat = false;
+                        //Playing Rocky (only throws rock)
+                        if (input == RPS.rock.ToString() && rocky.GenerateRPS() == RPS.rock)
+                        {
+                            Console.WriteLine($"{player1.Name}: {input}");
+                            Console.WriteLine($"{ rocky.Name}: rock");
+                            Console.WriteLine("Draw");
+                        }
+                        else if (input == RPS.scissors.ToString() && rocky.GenerateRPS() == RPS.rock)
+                        {
+                            Console.WriteLine($"{player1.Name}: {input}");
+                            Console.WriteLine($"{ rocky.Name}: rock");
+                            Console.WriteLine("You Lose");
+                        }
+                        else if (input == RPS.paper.ToString() && rocky.GenerateRPS() == RPS.rock)
+                        {
+                            Console.WriteLine($"{player1.Name}: {input}");
+                            Console.WriteLine($"{ rocky.Name}: rock");
+                            Console.WriteLine("You Win!");
+                        }
+                        playerSelect = false;
+                    }
+                    //Playing Randy. Throws a random hand.
+                    else if (opponent == "rand")
+                    {
+                        RandomPlayer randy = new RandomPlayer("Randy");
+                        Console.WriteLine("You chose to play a random player");
+                        Console.WriteLine("Please make a selection: Rock, paper, scissors");
+                        string input = Console.ReadLine().ToLower();
+
+                        if (input == RPS.rock.ToString() && randy.GenerateRPS() == RPS.rock)
+                        {
+                            Console.WriteLine($"{player1.Name}: {input}");
+                            Console.WriteLine($"{randy.Name}: rock");
+                            Console.WriteLine("Draw");
+                        }
+                        else if (input == RPS.scissors.ToString() && randy.GenerateRPS() == RPS.rock)
+                        {
+                            Console.WriteLine($"{player1.Name}: {input}");
+                            Console.WriteLine($"{randy.Name}: rock");
+                            Console.WriteLine("You Lose");
+                        }
+                        else if (input == RPS.paper.ToString() && randy.GenerateRPS() == RPS.rock)
+                        {
+                            Console.WriteLine($"{player1.Name}: {input}");
+                            Console.WriteLine($"{randy.Name}: rock");
+                            Console.WriteLine("You Win!");
+                        }
+                        else if (input == RPS.rock.ToString() && randy.GenerateRPS() == RPS.paper)
+                        {
+                            Console.WriteLine($"{player1.Name}: {input}");
+                            Console.WriteLine($"{randy.Name}: paper");
+                            Console.WriteLine("You lose...");
+                        }
+                        else if (input == RPS.scissors.ToString() && randy.GenerateRPS() == RPS.paper)
+                        {
+                            Console.WriteLine($"{player1.Name}: {input}");
+                            Console.WriteLine($"{randy.Name}: paper");
+                            Console.WriteLine("You Win!");
+                        }
+                        else if (input == RPS.paper.ToString() && randy.GenerateRPS() == RPS.paper)
+                        {
+                            Console.WriteLine($"{player1.Name}: {input}");
+                            Console.WriteLine($"{randy.Name}: paper");
+                            Console.WriteLine("Draw");
+                        }
+                        else if (input == RPS.rock.ToString() && randy.GenerateRPS() == RPS.scissors)
+                        {
+                            Console.WriteLine($"{player1.Name}: {input}");
+                            Console.WriteLine($"{randy.Name}: scissors");
+                            Console.WriteLine("You Win!");
+                        }
+                        else if (input == RPS.scissors.ToString() && randy.GenerateRPS() == RPS.scissors)
+                        {
+                            Console.WriteLine($"{player1.Name}: {input}");
+                            Console.WriteLine($"{randy.Name}: scissors");
+                            Console.WriteLine("Draw");
+                        }
+                        else if (input == RPS.paper.ToString() && randy.GenerateRPS() == RPS.scissors)
+                        {
+                            Console.WriteLine($"{player1.Name}: {input}");
+                            Console.WriteLine($"{randy.Name}: scissors");
+                            Console.WriteLine("You lose...");
+                        }
+                        playerSelect = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("That is not a valid choice. Please choose again");
+                        playerSelect = true;
+                    }
                 }
-                else if (opponent == "rand")
+
+                Console.WriteLine("Would you like to play again? (y/n)");
+                string select = Console.ReadLine().ToLower();
+
+                if (select == "y")
                 {
-                    
-                    RandomPlayer randy = new RandomPlayer("Randy");
-                    Console.WriteLine("You chose to play a random player");
-                    repeat = false;
+                    repeat = true;
                 }
                 else
                 {
-                    Console.WriteLine("That is not a valid choice. Please choose again");
-                    repeat = true;
-
-                }  
+                    Console.WriteLine("Goodbye!");
+                    repeat = false;
+                }
             }
             return players;
-        }
-        public static string PlayerName()
-        {
-            Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
-            return name;
+
+            //Method to take in and store the players name.
+             static string PlayerName()
+            {
+                Console.WriteLine("What is your name?");
+                string name = Console.ReadLine();
+                return name;
+            }
         }
     }
 }
